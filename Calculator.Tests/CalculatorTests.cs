a7905ea4815c46ef217fd38b95375443a788261e
Wendel <waguiartrader@gmail.com>
2021-06-17 19:10:35 -0300

    programa calculadora

---
+++ Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Shouldly;
+
+namespace Calculator.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ShouldReturnTheSumOfTheProvidedNumbers()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Add(1, 2);
+
+            // Assert
+            result.ShouldBe(3);
+        }
+
+        [Fact]
+        public void Subtract_ShouldReturnTheSubtractionOftheProvidedNumbers()
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Subtract(2, 1);
+
+            result.ShouldBe(1);
+        }
+
+        [Fact]
+        public void Divide_ShouldThrowExceptionWhenDividingByZero()
+        {
+            var calculator = new Calculator();
+
+            var exception = Should.Throw<Exception>(() => calculator.Divide(2, 0));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+<p align="center">
+    <a href="#bookmark-sobre-o-projeto">Sobre</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+    <a href="#rocket-tecnologias-utilizadas">Tecnologias</a>
+
+
+
+
+## :bookmark: Sobre o Projeto
+
+Calculadora simples para simular uma calculadora e testes unitarios com Xunit, Shouldly e Moq, criado em C# utilizando Visual Studio Community
+
+## :rocket: Tecnologias Utilizadas
+
+-  [C#](https://dotnet.microsoft.com/download)
+
